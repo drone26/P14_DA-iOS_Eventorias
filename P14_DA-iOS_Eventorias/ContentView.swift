8999bb05acdedad5fbdec2f9725e3263b5e0221b
Mathieu ARRIO <minimat26git@arrio.fr>
2026-06-11 10:45:49 +0200

    Initial Commit

---
+++ P14_DA-iOS_Eventorias/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  P14_DA-iOS_Eventorias
+//
+//  Created by Mathieu ARRIO on 11/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ P14_DA-iOS_Eventorias/P14_DA_iOS_EventoriasApp.swift
@@ -0,0 +1,17 @@
+//
+//  P14_DA_iOS_EventoriasApp.swift
+//  P14_DA-iOS_Eventorias
+//
+//  Created by Mathieu ARRIO on 11/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct P14_DA_iOS_EventoriasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
